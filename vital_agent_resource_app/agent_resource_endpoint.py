046c25cca8ee020fb5b2b811418be6b168e350a3
Marc Hadfield <marc@vital.ai>
2024-07-25 13:53:06 -0400

    sync

---
+++ vital_agent_resource_app/agent_resource_endpoint.py
@@ -0,0 +1,6 @@
+
+# rest endpoint implementation
+
+# kg service calls
+# text extract service calls
+# resource service calls, weather, etc,
---
+++ vital_agent_resource_app/impl/kgservice_impl.py
@@ -0,0 +1,13 @@
+from kgraphservice.kgraph_service_inf import KGraphServiceInterface
+
+
+class KGServiceImpl(KGraphServiceInterface):
+    pass
+
+# kg service implementation, wraps around underlying
+# kg service
+
+# input to include details of agent and user making request
+# include additional validation, constraints on queries, etc.
+
+
